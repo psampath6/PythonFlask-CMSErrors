a33635ecdab92f41073d2d58c5f8a2f5ead25e1d
Pradeep Sampath <pradeepsampath@gmail.com>
2021-02-08 22:14:55 -0800

    module completed

--- cms/handlers.py
+++ cms/handlers.py
@@ -60,5 +60,7 @@ def handle_exception(e):
     return render_template("error.html", error=original), 500
 
 unauthorized_log = configure_logging('unauthorized', WARN)
+
+@unauthorized.connect
 def log_unauthorized(app, user_id, username, **kwargs):
-    unauthorized_log.warning('Unauthorized; %s %s %s', timestamp, user_id, username)
+    unauthorized_log.warning('Unauthorized: %s %s %s', timestamp, user_id, username)
